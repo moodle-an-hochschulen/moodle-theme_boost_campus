52f7e3caf89560e4370720db79f96f80f49ad452
Schneider <oa73neve@ads.tu-darmstadt.de>
2018-04-25 11:27:46 +0200

    Fixes guest-enrol into courses

--- locallib.php
+++ locallib.php
@@ -269,14 +269,17 @@ function theme_boost_campus_process_flatnav(flat_navigation $flatnav) {
 function theme_boost_campus_set_node_on_top(flat_navigation $flatnav, $nodename, $beforenode) {
     // Get the node for which the sorting shall be changed.
     $pageflatnav = $flatnav->find($nodename, global_navigation::TYPE_SYSTEM);
-    // Set the showdivider of the new top node to false that no empty nav-element will be created.
-    $pageflatnav->set_showdivider(false);
-    // Add the showdivider to the coursehome node as this is the next one and this will add a margin top to it.
-    $beforenode->set_showdivider(true);
-    // Remove the site home navigation node that it does not appear twice in the menu.
-    $flatnav->remove($nodename);
-    // Add the saved site home node before the $beforenode.
-    $flatnav->add($pageflatnav, $beforenode->key);
+
+	if(is_null($pageflatnav)) {
+		// Set the showdivider of the new top node to false that no empty nav-element will be created.
+		$pageflatnav->set_showdivider(false);
+		// Add the showdivider to the coursehome node as this is the next one and this will add a margin top to it.
+		$beforenode->set_showdivider(true);
+		// Remove the site home navigation node that it does not appear twice in the menu.
+		$flatnav->remove($nodename);
+		// Add the saved site home node before the $beforenode.
+		$flatnav->add($pageflatnav, $beforenode->key);
+	}
 
     // Return the modified changes.
     return $flatnav;

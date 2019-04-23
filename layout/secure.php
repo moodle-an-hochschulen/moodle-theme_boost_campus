9470287a6b66b25bff2d68b8084cc6e6993ce15c
Trevor Cunningham <trevor.cunningham@uregina.ca>
2019-04-23 14:07:05 -0600

    adding support for secure layout (used with quizzes)

---
+++ layout/secure.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A secure layout for the boost theme.
+ *
+ * @package   theme_boost
+ * @copyright 2016 Damyon Wiese
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = strpos($blockshtml, 'data-block=') !== false;
+$bodyattributes = $OUTPUT->body_attributes();
+// MODIFICATION START: Setting 'darknavbar'.
+if (get_config('theme_boost_campus', 'darknavbar') == 'yes') {
+    $darknavbar = true;
+} else {
+    $darknavbar = false;
+}
+
+$sitecontextheader = '<div class="page-context-header"><div class="page-header-headings"><h1>'.$COURSE->fullname.'</h1></div></div>';
+$headertext = (!empty($this->context_header())) ? $this->context_header() : $sitecontextheader;
+
+$templatecontext = [
+    'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+    'output' => $OUTPUT,
+    'bodyattributes' => $bodyattributes,
+    'sidepreblocks' => $blockshtml,
+    'hasblocks' => $hasblocks,
+    'darknavbar' => $darknavbar,
+	'contextheader' => $headertext;
+];
+
+echo $OUTPUT->render_from_template('theme_boost_campus/secure', $templatecontext);
+

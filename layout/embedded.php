9eb7e5a53a85cedee1053f44986f3dc9ed4d3673
Kira Siganski <k.siganski@outlook.com>
2019-05-24 15:52:28 -0600

    darkmode compatability with embedded pages, CSS patches for unruly lists

---
+++ layout/embedded.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * An embedded layout for the boost theme.
+ *
+ * @package   theme_boost
+ * @copyright 2016 Damyon Wiese
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG,$PAGE,$DB;
+//check if user has darkmode on in database and include if so
+if($DB->get_record('theme_boost_campus_darkmode', array('userid'=>$USER->id, 'darkmode'=>1))){
+    $PAGE->requires->css('/theme/boost_campus/css/darkmode.css');
+ }
+
+$templatecontext = [
+    'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+    'output' => $OUTPUT
+];
+
+echo $OUTPUT->render_from_template('theme_boost/embedded', $templatecontext);

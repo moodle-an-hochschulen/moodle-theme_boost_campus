b48054d25004859c0eee65c79cbc172b52d9a77d
Trevor Cunningham <trevor.cunningham@uregina.ca>
2019-06-13 15:16:48 -0600

    bumping version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_urcourses_default';
-$plugin->version = 2019051602;
+$plugin->version = 2019061300;
 $plugin->release = 'v3.6-r2';
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;

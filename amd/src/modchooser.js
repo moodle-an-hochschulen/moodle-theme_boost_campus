f3fc30e9acd3dda048ebdae2f07c30cd91ed669c
Trevor Cunningham <trevor.cunningham@uregina.ca>
2019-08-19 15:37:25 -0600

    remove console logging

--- amd/src/modchooser.js
+++ amd/src/modchooser.js
@@ -3,22 +3,21 @@ define(['jquery'], function($){
     * Snap modchooser listener to add current section to urls.
      */
     var modchooserjump = function() {
-        console.log("modchooser jump");
+        //console.log("modchooser jump");
         $("[name='submitbutton']").trigger('click');
     };
 
     var setmodjump = function() {
-        console.log("clicked");
+        //console.log("clicked");
         setTimeout(function(){
-            console.log("timed out");
+            //console.log("timed out");
             $('li.modchooser_resources a').tab('show');
             $("[type='radio']").change(modchooserjump);
         }, 500);
     };
 
-    
     var init = function() {
-        console.log("mod chooser init");
+        //console.log("mod chooser init");
         //create an event listener on the hidden input named "jump"
         $('.section-modchooser-link').click(setmodjump);
     };

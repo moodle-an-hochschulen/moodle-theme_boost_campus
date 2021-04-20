07759fb66b8e40a22453a68ee0bfd9587d2f3d33
Alexander Bias <alexander.bias@uni-ulm.de>
2021-04-20 09:07:36 +0200

    Release v3.10-r6

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_campus';
-$plugin->version = 2020112804;
-$plugin->release = 'v3.10-r5';
+$plugin->version = 2020112805;
+$plugin->release = 'v3.10-r6';
 $plugin->requires = 2020110900;
 $plugin->supported = [310, 310];
 $plugin->maturity = MATURITY_STABLE;

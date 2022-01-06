b8cbfdaba791d6021da7f816679267f481a16394
Alexander Bias <bias@alexanderbias.de>
2022-01-06 13:17:51 +0100

    Release v3.11-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_campus';
-$plugin->version = 2021122800;
-$plugin->release = 'v3.11-r1';
+$plugin->version = 2021122801;
+$plugin->release = 'v3.11-r2';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
